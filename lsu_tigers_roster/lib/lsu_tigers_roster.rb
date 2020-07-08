d7e68e527676c0d8675448e5cf69ec22aba21bfc
Jeremy Bourne <jbourne25@yahoo.com>
2020-07-08 02:22:20 +0000

    Automatically backed up by Learn

--- lsu_tigers_roster/lib/lsu_tigers_roster.rb
+++
@@ -1,6 +0,0 @@
-require "lsu_tigers_roster/version"
-
-module LsuTigersRoster
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lsu_tigers_roster/lib/lsu_tigers_roster/version.rb
+++
@@ -1,3 +0,0 @@
-module LsuTigersRoster
-  VERSION = "0.1.0"
-end
